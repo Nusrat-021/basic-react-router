dfeca905657f0a3b036b53e1be2c4066765313ec
Tanha-013 <nusrattanha897@gmail.com>
2023-09-23 12:56:06 +0600

    useNavigate and navigate function

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
+    const navigate = useNavigate();
+        console.log(navigate);
     const {title,id} = post;
+
+    const handleShowDetails = () => {
+        navigate(`/post/${id}`)
+    }
     return (
         <div>
             <h1>{title}</h1>
             <Link to={`/post/${id}`}>
                 <button>Show Details</button>
             </Link>
+            <button onClick={handleShowDetails}>Click to see more details</button>
         </div>
     );
 };
--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,15 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
     const post = useLoaderData();
     console.log(post);
     const {userId,title,body} = post;
+    const navigate = useNavigate();
+
+    const handleGoBack = () =>{
+        // navigate(-1);
+        navigate('/posts');
+    }
     return (
         <div>
             <h1>{title}</h1>
             <h3>{body}</h3>
             <h5>UserId: {userId}</h5>
+            <button onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
